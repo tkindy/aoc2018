e5275132f837000f0b6f9c0636664feb294d3ed6
Tyler Kindy <tylerkindy@gmail.com>
2018-12-06 18:44:10 -0500

    Use single, mutable set for past sums

--- src/main/kotlin/com/tylerkindy/aoc2018/day1/Day1.kt
+++ src/main/kotlin/com/tylerkindy/aoc2018/day1/Day1.kt
@@ -7,43 +7,31 @@ fun main() {
     val freqs = parseFrequencies(data)
 
     println("One-run sum: ${freqs.sum()}")
-    println("Repeat: ${findRepeat(freqs).sum}")
+    val repeat = findRepeat(freqs)
+    println("Repeat: ${repeat.sum} (after ${repeat.pastSums.size} sums)")
 }
 
-fun parseFrequencies(data: String): List<Freq> {
+fun parseFrequencies(data: String): List<Int> {
     return data.split('\n')
         .filter(String::isNotEmpty)
         .map(String::toInt)
 }
 
-fun findRepeat(freqs: List<Freq>): Repeat {
-    var curSum: Freq = 0
+fun findRepeat(freqs: List<Int>): Repeat {
+    var curSum = 0
     var index = 0
-
-    val sumSets = (0 until NUM_SETS).map {
-        mutableSetOf<Freq>()
-    }
+    val pastSums = mutableSetOf<Int>()
 
     while (true) {
-        val setIndex = sumIndex(curSum)
-        if (curSum in sumSets[setIndex]) {
-            return Repeat(curSum, sumSets.fold(emptySet()) { allSums, curSet -> allSums + curSet })
+        if (curSum in pastSums) {
+            return Repeat(curSum, pastSums)
         }
 
-        sumSets[setIndex] += curSum
+        pastSums += curSum
         curSum += freqs[index]
         index = (index + 1) % freqs.size
     }
 }
 
-fun sumIndex(sum: Freq): Int {
-    return (sum + MAX_VAL) / ((2 * MAX_VAL) / NUM_SETS)
-}
-
-private const val NUM_SETS = 100
-private const val MAX_VAL = 100_000
-
 // For testing
-data class Repeat(val sum: Freq, val pastSums: Set<Freq>)
-
-typealias Freq = Int
+data class Repeat(val sum: Int, val pastSums: Set<Int>)
